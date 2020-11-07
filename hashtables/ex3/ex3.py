7a5bd793fa18a59a3e83e748bf494997b257d6b9
Wade-Coplen <wadecoplen@gmail.com>
2020-11-07 13:04:20 -0700

    dnf

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,15 +1,42 @@
+import random
 def intersection(arrays):
     """
     YOUR CODE HERE
-    """
-    # Your code here
     #UNDERSTAND
     #find all of the numbers that repeat in each list let return those repeated integers
     #PLAN
     # To do this, we would need to iterate over each list and assign each
     # integer a key so that every time that integer is encountered it is add to that key as a value?
+    #EXECUTE
+    # [
+    #   [0]
+    #   [1]
+    #   [2]
+    # ]
+    # If we set a 
+    # set a value in a dict with [ ]
+        # item = {}
+        # item['a'] = 'alpha'
+    # dict ={}
 
+    # what would we use as a key?
+    """
+    # Your code here
+    result = {}
+    
+    
+    for list in arrays:
+        for element in list:
+            if element not in result:
+                result[element] = 1
+            else:
+                result[element] += 1
+                
     return result
+
+        
+       
+    
 
 
 if __name__ == "__main__":
